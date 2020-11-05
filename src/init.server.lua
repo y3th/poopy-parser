4115011dd14f07b85fd87c764f6dfd3c8e3455d3
y3th <y3thy3th@gmail.com>
2020-11-05 17:02:32 -0500

    Update init.server.lua

--- src/init.server.lua
+++ src/init.server.lua
@@ -1,1 +1,5 @@
-print("Hello world!")
+local parse = require(script.PoopyParse)
+local build = require(script.PoopyBuild)
+
+local test = script.tests.test.Value
+build(parse(test)).Parent = workspace
